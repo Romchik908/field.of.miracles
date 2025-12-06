57fc1d85d1f8e564c7bd6037a996e4e7e47da86d
rmalyshkin <rmalyshkin@skbkontur.ru>
2025-12-07 01:20:56 +0500

    главная страница

--- src/components/IntroScreen/IntroScreen.tsx
+++ src/components/IntroScreen/IntroScreen.tsx
@@ -2,6 +2,7 @@ import { Button } from '@skbkontur/react-ui';
 import React from 'react';
 import { useGameContext } from '../../context/GameContext';
 import { hasSavedGame } from '../../utils/storage';
+import { IntroBackground } from '../IntroBackground/IntroBackground';
 import styles from './IntroScreen.module.scss';
 
 export const IntroScreen: React.FC = () => {
@@ -10,32 +11,36 @@ export const IntroScreen: React.FC = () => {
 
   const titleWord1 = 'Поле';
   const titleWord2 = 'Чудес';
+  // Обновленный текст
+  const subtitleText = 'ЭКСТЕРН ШОУ';
 
-  const subtitleText = 'КАПИТАЛ ШОУ';
-
+  // Рендер букв заголовка (Поле Чудес)
   const renderTitleChars = (word: string, startDelay: number) => {
     return word.split('').map((char, index) => (
       <span
         key={`${word}-${index}`}
         className={styles.char}
-        style={{ animationDelay: `${startDelay + index * 0.2}s` }}
+        style={{ animationDelay: `${startDelay + index * 0.1}s` }}
       >
         {char}
       </span>
     ));
   };
 
+  // Рендер букв подзаголовка
   const renderSubtitleChars = () => {
-    const baseDelay = 2.5;
+    // Начинаем анимацию, когда заголовок уже появился (через ~1.5с)
+    const baseDelay = 1.5;
 
     return subtitleText.split('').map((char, index) => {
-      if (char === ' ') return <span key={index} style={{ width: '20px' }}></span>;
+      // Пробел оставляем как есть
+      if (char === ' ') return <span key={index} style={{ width: '15px' }}></span>;
 
       return (
         <span
           key={index}
           className={styles.subChar}
-          style={{ animationDelay: `${baseDelay + index * 0.1}s` }}
+          style={{ animationDelay: `${baseDelay + index * 0.05}s` }}
         >
           {char}
         </span>
@@ -44,30 +49,39 @@ export const IntroScreen: React.FC = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.rays} />
-      <div className={styles.contentWrapper}>
-        <div className={styles.title}>
-          <div style={{ display: 'flex' }}>{renderTitleChars(titleWord1, 0.5)}</div>
-          <div style={{ display: 'flex' }}>
-            {renderTitleChars(titleWord2, 0.5 + titleWord1.length * 0.2)}
+    <IntroBackground>
+      <div className={styles.container}>
+        <div className={styles.contentWrapper}>
+          {/* Логотип: Playfair Display */}
+          <div className={styles.title}>
+            <div style={{ display: 'flex' }}>{renderTitleChars(titleWord1, 0.2)}</div>
+
+            <div style={{ display: 'flex' }}>
+              {renderTitleChars(titleWord2, 0.2 + titleWord1.length * 0.1)}
+            </div>
           </div>
+
+          {/* Подзаголовок: Lab Grotesque (Montserrat) */}
+          <div className={styles.subtitle}>{renderSubtitleChars()}</div>
         </div>
-        <div className={styles.subtitle}>{renderSubtitleChars()}</div>
-      </div>
-      <div className={styles.menu}>
-        {canContinue && (
-          <Button onClick={continueGame} use="primary" size="large" width="100%">
-            ПРОДОЛЖИТЬ
+
+        {/* Меню кнопок */}
+        <div className={styles.menu}>
+          {canContinue && (
+            <Button onClick={continueGame} use="primary" size="large" width="100%">
+              ПРОДОЛЖИТЬ
+            </Button>
+          )}
+
+          <Button onClick={startNewGame} size="large" width="100%">
+            НОВАЯ ИГРА
           </Button>
-        )}
-        <Button onClick={startNewGame} size="large" width="100%">
-          НОВАЯ ИГРА
-        </Button>
-        <Button onClick={goToManualSetup} size="medium" width="100%">
-          НАСТРОИТЬ РАУНД
-        </Button>
+
+          <Button onClick={goToManualSetup} size="medium" width="100%">
+            НАСТРОИТЬ РАУНД
+          </Button>
+        </div>
       </div>
-    </div>
+    </IntroBackground>
   );
 };
